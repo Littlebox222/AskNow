4b70c905d8ad4f4d57987d9981634a81d867b7de
Littlebox222 <Littlebox222@gmail.com>
2017-02-20 17:14:07 +0800

    init

---
+++ AskNow/AskNow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AskNow
+//
+//  Created by Littlebox on 20/02/2017.
+//  Copyright © 2017 PandaLearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AskNow/AskNow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AskNow
+//
+//  Created by Littlebox on 20/02/2017.
+//  Copyright © 2017 PandaLearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
